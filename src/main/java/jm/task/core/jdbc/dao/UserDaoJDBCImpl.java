224ffd9a8205c51c96e7d9df338ce6d497e0cde4
IvanKrasikov <98708823+IvanKrasikov@users.noreply.github.com>
2023-07-24 20:27:08 +0300

    Add files via upload

---
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -0,0 +1,75 @@
+package jm.task.core.jdbc.dao;
+
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDaoJDBCImpl implements UserDao {
+    public UserDaoJDBCImpl() {
+
+    }
+
+    public void createUsersTable() {
+        try (Statement statement = new Util().getConn().createStatement()) {
+            statement.executeUpdate("CREATE TABLE users (id INT PRIMARY KEY AUTO_INCREMENT, " +
+                    "name VARCHAR(255), lastName VARCHAR(255), age INT)");
+        } catch (SQLException e) {
+            //System.out.println("NOT CREATE TABLE");
+        }
+    }
+
+    public void dropUsersTable() {
+        try (Statement statement = new Util().getConn().createStatement()) {
+            statement.executeUpdate("DROP TABLE users");
+        } catch (SQLException e) {
+            //System.out.println("NOT DROP TABLE");
+        }
+    }
+
+    public void saveUser(String name, String lastName, byte age) {
+        try (Statement statement = new Util().getConn().createStatement()) {
+            statement.executeUpdate("INSERT users(name, lastName, age) VALUE(\""
+                    + name + "\", \"" + lastName + "\", " + age + ")");
+        } catch (SQLException e) {
+            //System.out.println("NOT SAVE USER");
+        }
+    }
+
+    public void removeUserById(long id) {
+        try (Statement statement = new Util().getConn().createStatement()) {
+            statement.executeUpdate("DELETE FROM users WHERE id = " + id);
+        } catch (SQLException e) {
+
+        }
+    }
+
+    public List<User> getAllUsers() {
+        List<User> users = new ArrayList<>();
+        try (Statement statement = new Util().getConn().createStatement()) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
+            while (resultSet.next()) {
+                User user = new User(resultSet.getString("name"),
+                        resultSet.getString("lastName"),
+                        resultSet.getByte("age")
+                        );
+                user.setId(resultSet.getLong("id"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+
+        }
+        return users;
+    }
+
+    public void cleanUsersTable() {
+        try (Statement statement = new Util().getConn().createStatement()) {
+            statement.executeUpdate("DELETE FROM users WHERE id > 0");
+        } catch (SQLException e) {
+
+        }
+    }
+}
---
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -0,0 +1,28 @@
+package jm.task.core.jdbc.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Util {
+    // реализуйте настройку соеденения с БД
+    private final static String URL = "jdbc:mysql://localhost:3306/pre_project";
+    private final static String USER = "root";
+    private final static String PASS = "root";
+    private static Connection conn = null;
+
+    public Util() throws SQLException {
+            conn = DriverManager.getConnection(URL, USER, PASS);
+//            if (!conn.isClosed()) {
+//                System.out.println("Connection");
+//            }
+    }
+
+    public Util(String url, String user, String pass) throws SQLException {
+            conn = DriverManager.getConnection(url, user, pass);
+    }
+
+    public Connection getConn() {
+        return conn;
+    }
+}
